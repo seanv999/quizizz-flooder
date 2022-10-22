2336ae33ee0c529295e7712a336d14df6e0da48f
Sean Vonhauger <79374771+seanv999@users.noreply.github.com>
2022-10-22 22:54:10 +0000

    Open Source ✔
    Made The Code Open Source!

--- flood.js
+++ flood.js
@@ -1,1 +1,35 @@
-const entryPoint=Object.values(document.querySelector("#root"))[1]._context.provides.store,client={hostId:JSON.parse(JSON.stringify(entryPoint.getState())).game.data.hostId,roomHash:JSON.parse(JSON.stringify(entryPoint.getState())).game.data.roomHash,localPlayer:JSON.parse(localStorage.previousContext).currentPlayer.playerId},flood=(o,t)=>{for(i=0;i<t;i++)entryPoint._actions.connectSocket[0](),entryPoint._actions.joinRoom[0]({roomHash:client.roomHash,playerId:`${o}-${i}`})},n=prompt("Name of bots: "),a=parseInt(prompt("How Many Bots: "));flood(n,a);
+//Sean Vonhauger
+let a;
+let n;
+const input = () => {
+    n = prompt("Name of bots: ")
+    if(n == ''){
+        alert('Name Needs A Value!')
+        throw('Name Needs A Value!')
+    }
+    a = parseInt(prompt("How Many Bots: "));
+    if(a <= 0 || a == NaN){
+        alert('Amount Needs A Valid Number!')
+        throw('Amount Needs A Valid Number!')
+    }
+  
+}
+
+const entryPoint = Object.values(document.querySelector("#root"))[1]._context.provides.store
+client = {
+    hostId: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.hostId,
+    roomHash: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.roomHash,
+    localPlayer: JSON.parse(localStorage.previousContext).currentPlayer.playerId
+}
+
+const flood = (o, t) => {
+    for (i = 0; i < t; i++){
+        entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
+        roomHash: client.roomHash,
+        playerId: `${o}-${i}`});
+        console.log(`${i}/${t} Bots Connected!`)
+    }
+}
+
+input()
+flood(n, a)
